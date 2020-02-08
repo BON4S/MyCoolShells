4a63b94be00d2e4e3c1deb3491dc6b92d3e45212
BON4S <prezadorodrigo@gmail.com>
2020-02-08 13:01:51 -0300

    Obeying the shell and javascript naming convention

--- news_page/news_page.sh
+++ news_page/news_page.sh
@@ -55,71 +55,71 @@ while [ -n "$1" ]; do
 done
 
 # this takes news from a website
-ReadFeed() {
-  urlNews=$2
-  pageContent=$(wget --timeout=5 --tries=2 -O- ${urlNews}) 
-  IFS=$'\n'; title=($(xmllint --xpath '//item/title/text()' - <<<"$pageContent"))     # extract the title
-  IFS=$'\n'; link=($(xmllint --xpath '//item/link/text()' - <<<"$pageContent"))       # extract the link
+feed() {
+  url=$2
+  content=$(wget --timeout=5 --tries=2 -O- ${url}) 
+  IFS=$'\n'; title=($(xmllint --xpath '//item/title/text()' - <<<"$content"))     # extract the title
+  IFS=$'\n'; link=($(xmllint --xpath '//item/link/text()' - <<<"$content"))       # extract the link
   echo -ne "<div class='news'><h2 class='news-site'>$1</h2>"
   if [ -z "$3" ]; then
-    newsLimit=6             # limits the amount of news displayed by each site
+    limit=6             # limits the amount of news displayed by each site
   else
-    newsLimit=$3            # you can specify the amount of news for an individual link (third parameter)
+    limit=$3            # you can specify the amount of news for an individual link (third parameter)
   fi
-  for ((i=0; i<$newsLimit; ++i)) do
+  for ((i=0; i<$limit; ++i)) do
     echo -ne "<h3 class='news-titles'><a href='${link[i]}'>${title[i]}</a></h3>"
   done
   echo "</div>"
 }
 
 # get tweets
-Twitter() {
-  urlTwitter="https://twitter.com/$2"
-  pageContent=$(wget --timeout=5 --tries=2 -O- ${urlTwitter})
-  echo -ne "<div class='tweets'><h2><a href='$urlTwitter'>$1</a></h2>"
+twitter() {
+  url="https://twitter.com/$2"
+  content=$(wget --timeout=5 --tries=2 -O- ${url})
+  echo -ne "<div class='tweets'><h2><a href='$url'>$1</a></h2>"
   echo -ne "<div class='tweets-box brightness'>"
-  echo -e $pageContent | pup 'div.tweet'
+  echo -e $content | pup 'div.tweet'
   echo -ne"</div></div>"
 }
 
 # this takes the value of one currency and converts it to another currency
-CurrencyConverter() {
-  targetCurrencyNumber=$(wget -qO- "https://api.exchangeratesapi.io/latest?base=$1" | grep -Eo "$2\":[0-9.]*" | grep -Eo "[0-9.]*") > /dev/null
-  echo "1 $1 = ${targetCurrencyNumber:0:4} $2"
+currency() {
+  value=$(wget -qO- "https://api.exchangeratesapi.io/latest?base=$1" | grep -Eo "$2\":[0-9.]*" | grep -Eo "[0-9.]*") > /dev/null
+  echo "1 $1 = ${value:0:4} $2"
 }
 
 # stylization
-cssDefault=`cat news_page.css`   # css that is applied to all themes (light and dark)
+css=`cat news_page.css`   # css that is applied to all themes (light and dark)
 if [ "$color" == "dark" ]; then
-  cssColor=`cat news_page_dark_theme.css`    # dark theme css
+  theme=`cat news_page_dark_theme.css`    # dark css theme
 else
-  cssColor=`cat news_page_light_theme.css`   # light theme css
+  theme=`cat news_page_light_theme.css`   # light css theme
 fi
 
 # html top code
-topCode="
+top="
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>NEWS</title>
-    <style>$cssDefault$cssColor</style>
+    <style>$css$theme</style>
   </head>
   <body>
     <div id='content'>
 "
 
 # html bottom code
-bottomCode="
+bottom="
     </div>
   </body>
 </html>
 "
 
 # start of news page generator code
-HtmlGenerator() {
-  echo "$topCode"
+generator() {
+  echo "$top"
 
   # top line
   echo "
@@ -129,7 +129,7 @@ HtmlGenerator() {
     <div>
       <p><b>Currency</b> &nbsp;
   "
-  CurrencyConverter USD BRL
+  currency USD BRL
   echo "
       </p>
     </div>
@@ -155,7 +155,7 @@ HtmlGenerator() {
     <div class='right'>
       <span class='control'>
         <a href='#top' onclick='newsBrightness()'>☀</a>
-        <a href='#top' onclick='FullScreen()'>⤢</a>
+        <a href='#top' onclick='fullScreen()'>⤢</a>
         <a href='#line1'>↓</a>
       </span>
     </div>
@@ -166,7 +166,7 @@ HtmlGenerator() {
   # full screen and news brightness
   echo "
   <script>
-    function FullScreen() {
+    function fullScreen() {
       if (!document.fullscreenElement) {
         document.documentElement.requestFullscreen();
     } else {
@@ -186,35 +186,35 @@ HtmlGenerator() {
 
   # news section
 # ----
-  ReadFeed "InfoMoney" "https://www.infomoney.com.br/feed/"
-  ReadFeed "Google News BR" "https://news.google.com/rss?cf=all&pz=1&hl=pt-BR&gl=BR&ceid=BR:pt-419"
-  ReadFeed "The Wall Street Journal" "https://feeds.a.dj.com/rss/RSSWorldNews.xml"
-  ReadFeed "Trend Topics BR" "https://trends.google.com/trends/trendingsearches/daily/rss?geo=BR" "10"
+  feed "InfoMoney" "https://www.infomoney.com.br/feed/"
+  feed "Google News BR" "https://news.google.com/rss?cf=all&pz=1&hl=pt-BR&gl=BR&ceid=BR:pt-419"
+  feed "The Wall Street Journal" "https://feeds.a.dj.com/rss/RSSWorldNews.xml"
+  feed "Trend Topics BR" "https://trends.google.com/trends/trendingsearches/daily/rss?geo=BR" "10"
 # ----
-  ReadFeed "Money Times" "https://moneytimes.com.br/feed/"
-  ReadFeed "Orange County Register" "https://www.ocregister.com/news/feed/"
-  ReadFeed "CBS8" "https://feeds.feedblitz.com/cbs8/news&x=1"
-  ReadFeed "Gazeta News" "https://gazetanews.com/feed/?post_type=jm_breaking_news"
+  feed "Money Times" "https://moneytimes.com.br/feed/"
+  feed "Orange County Register" "https://www.ocregister.com/news/feed/"
+  feed "CBS8" "https://feeds.feedblitz.com/cbs8/news&x=1"
+  feed "Gazeta News" "https://gazetanews.com/feed/?post_type=jm_breaking_news"
 # ----
-  ReadFeed "Times of India" "https://timesofindia.indiatimes.com/rssfeedstopstories.cms"
-  ReadFeed "TASS" "http://tass.com/rss/v2.xml"
-  ReadFeed "The Japan Times" "https://www.japantimes.co.jp/feed/topstories"
-  ReadFeed "Público (Portugal)" "http://feeds.feedburner.com/PublicoRSS"
+  feed "Times of India" "https://timesofindia.indiatimes.com/rssfeedstopstories.cms"
+  feed "TASS" "http://tass.com/rss/v2.xml"
+  feed "The Japan Times" "https://www.japantimes.co.jp/feed/topstories"
+  feed "Público (Portugal)" "http://feeds.feedburner.com/PublicoRSS"
 # ----
-  ReadFeed "Diolinux" "https://www.diolinux.com.br/feeds/posts/default?alt=rss"
-  ReadFeed "Phoronix" "https://www.phoronix.com/rss.php"
-  ReadFeed "TecMundo" "https://rss.tecmundo.com.br/feed"
-  ReadFeed "Arch Linux Brasil" "https://www.archlinux-br.org/feeds/news/"
+  feed "Diolinux" "https://www.diolinux.com.br/feeds/posts/default?alt=rss"
+  feed "Phoronix" "https://www.phoronix.com/rss.php"
+  feed "TecMundo" "https://rss.tecmundo.com.br/feed"
+  feed "Arch Linux Brasil" "https://www.archlinux-br.org/feeds/news/"
 # ----
-  ReadFeed "Porto Imagem" "https://portoimagem.wordpress.com/feed/"
-  ReadFeed "Sociedade Militar" "https://www.sociedademilitar.com.br/wp/feed"
-  ReadFeed "Suno Notícias" "https://www.sunoresearch.com.br/noticias/feed/"
-  ReadFeed "Trend Topics US" "https://trends.google.com/trends/trendingsearches/daily/rss?geo=US" "10"
+  feed "Porto Imagem" "https://portoimagem.wordpress.com/feed/"
+  feed "Sociedade Militar" "https://www.sociedademilitar.com.br/wp/feed"
+  feed "Suno Notícias" "https://www.sunoresearch.com.br/noticias/feed/"
+  feed "Trend Topics US" "https://trends.google.com/trends/trendingsearches/daily/rss?geo=US" "10"
 # ----
-  ReadFeed "Gizmodo BR" "https://gizmodo.uol.com.br/feed/"
-  ReadFeed "HypeScience" "https://hypescience.com/feed/"
-  ReadFeed "Folha de SP" "http://feeds.folha.uol.com.br/emcimadahora/rss091.xml"
-  ReadFeed "The New York Times" "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+  feed "Gizmodo BR" "https://gizmodo.uol.com.br/feed/"
+  feed "HypeScience" "https://hypescience.com/feed/"
+  feed "Folha de SP" "http://feeds.folha.uol.com.br/emcimadahora/rss091.xml"
+  feed "The New York Times" "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
 # ----
 
   # twitter top line
@@ -228,7 +228,7 @@ HtmlGenerator() {
       <div class='right'>
         <span class='control'>
           <a href='#line1' onclick='twitterBrightness()'>☀</a>
-          <a href='#line1' alt='Full Screen' onclick='FullScreen()'>⤢</a>
+          <a href='#line1' alt='Full Screen' onclick='fullScreen()'>⤢</a>
           <a href='#top'>↑</a>
         </span>
         <a href='#line1' onclick='twitterShowHide()'>Twitter Show/Hide</a>
@@ -252,27 +252,27 @@ HtmlGenerator() {
 
   # get tweets
   echo "<div id='the-twitter' class='hide'>"
-  Twitter "President of Brazil - Jair Bolsonaro" "jairbolsonaro"
-  Twitter "President of United States - Donald Trump" "realDonaldTrump"
-  Twitter "President of Argentina - Alberto Fernández" "alferdez"
+  twitter "President of Brazil - Jair Bolsonaro" "jairbolsonaro"
+  twitter "President of United States - Donald Trump" "realDonaldTrump"
+  twitter "President of Argentina - Alberto Fernández" "alferdez"
   echo "</div>" # id: the-twitter
 
 # FEED LINKS THAT DIDN'T WORK (FIXME)
-#  ReadFeed "The Hindu" "https://www.thehindu.com/feeder/default.rss"
-#  ReadFeed "Aljazera" "https://www.aljazeera.com/xml/rss/all.xml"
-#  ReadFeed "Xinhua" "http://www.xinhuanet.com/english/rss/chinarss.xml"
-#  ReadFeed "Jornal do Comércio" "https://www.jornaldocomercio.com/_conteudo/economia/rss.xml"
-#  ReadFeed "DefesaNet" "http://www.defesanet.com.br/capa/rss/"
-#  ReadFeed "East-West" "https://www.ewdn.com/feed/"
-#  ReadFeed "Financial Times" "https://www.ft.com/news-feed?format=rss"
-#  ReadFeed "El País BR" "https://brasil.elpais.com/rss/brasil/portada.xml"
+#  feed "The Hindu" "https://www.thehindu.com/feeder/default.rss"
+#  feed "Aljazera" "https://www.aljazeera.com/xml/rss/all.xml"
+#  feed "Xinhua" "http://www.xinhuanet.com/english/rss/chinarss.xml"
+#  feed "Jornal do Comércio" "https://www.jornaldocomercio.com/_conteudo/economia/rss.xml"
+#  feed "DefesaNet" "http://www.defesanet.com.br/capa/rss/"
+#  feed "East-West" "https://www.ewdn.com/feed/"
+#  feed "Financial Times" "https://www.ft.com/news-feed?format=rss"
+#  feed "El País BR" "https://brasil.elpais.com/rss/brasil/portada.xml"
 
-  echo "$bottomCode"
+  echo "$bottom"
 };  # end of news page generator code
 
 # news page generator launcher
 echo -e "\n GENERATING THE NEWS PAGE..\n"
-HtmlGenerator > $file
+generator > $file
 
 # la grande finale (French)
 echo -e "\n DONE!\n"
