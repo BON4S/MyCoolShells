8254fa93d1e8ee50099ac2d540f9dac841e3d1f9
BON4S <prezadorodrigo@gmail.com>
2020-02-29 21:40:51 -0300

    support for multiple events on the same day

--- google_calendar.sh
+++ google_calendar.sh
@@ -6,13 +6,13 @@
 # This little script captures data from my Google Calendar via gcalcli.
 # I use it to print my appointments on desktop (using Conky).
 
-gcalcli() {
+gcalcli-run() {
   initialdate=$(date +%Y-%m-%d)
   enddate=$(date +%Y-%m-%d -d "+ 15 day")   # stipulates the last 15 days for data capture
   gcalcli --nocolor agenda $initialdate $enddate
 }
 
-IFS=$'\n'; events=($(gcalcli))                        # capture the Google Calendar data
+IFS=$'\n'; events=($(gcalcli-run))                    # capture the Google Calendar data
 for n in "${!events[@]}"; do
   day=$(echo -ne ${events[n]} | cut -c 9-10)          # separate the day from captured
   month=$(echo -ne ${events[n]} | cut -c 5-7)         # month
@@ -24,11 +24,15 @@ for n in "${!events[@]}"; do
   elif [[ $time = *[!\ ]* ]]; then                    # check if there is a set time
     time=$(echo -ne " - $time")
     description=$(echo -ne ${events[n]} | cut -c28-)  # appointment title
-    echo -e " "$day"/"$month", "$week$time
-    echo -e " "$description"\n "
+    case $day in
+      ''|*[!0-9]*) echo -e " on the same day$time\n "$description"\n " ;;
+      *) echo -e " "$day"/"$month", "$week$time"\n "$description"\n " ;;
+    esac
   else
-    description=$(echo -ne ${events[n]} | cut -c20-)  # appointment title
-    echo -e " "$day"/"$month", "$week
-    echo -e " "$description"\n "
+    description=$(echo -ne ${events[n]} | cut -c20-)
+    case $day in
+      ''|*[!0-9]*) echo -e " on the same day\n "$description"\n " ;;
+      *) echo -e " "$day"/"$month", "$week"\n "$description"\n " ;;
+    esac
   fi
 done
