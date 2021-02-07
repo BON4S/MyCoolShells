79adb7fe6d4c0ed7f6f9f1066b96bc264b17d48c
BON4S <prezadorodrigo@gmail.com>
2021-02-07 18:53:19 +0000

    Switching from Yay to Paru.

--- update_arch.sh
+++ update_arch.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 # SCRIPT: update_arch.sh
 # AUTHOR: BON4S https://github.com/BON4S
-#
-# DESCRIPTION:
-# This script is a good way to update Arch without errors during the process.
+# DESCRIPTION: This script is an easy way to update Arch Linux.
+
+# For this script to work it is necessary to have 'default.sh' in the same folder.
 
 clear
 # --- default start ---------------------------
-cd $(dirname "$0")                                  # goes to the script folder
-source "default.sh"                                 # import default.sh
-top() { title "UPDATE ARCH LINUX$lred!"; }; top     # show the title
+cd $(dirname "$0")                                                    # goes to the script folder
+source "default.sh"                                                   # import default.sh
+top() { title "UPDATE AND MAINTENANCE OF ARCH LINUX$lred!"; }; top    # show the title
 # --- end of default start --------------------
+
+# stylized message
+message() {
+echo -e "\n$bold $1$reset$gray \n"
+}
 
 # message that appears at the end of each step
 finish() {
@@ -21,7 +26,7 @@ finish() {
 # you need to install and configure the 'newsboat' to this work
 news() {
   line
-  echo -e "\n$bold TIME TO READ ARCH LINUX UPDATE NEWS$reset$gray \n"
+  message "TIME TO READ ARCH LINUX UPDATE NEWS"
   echo -e " starting newsboat..$black ";
   if [ "$the_terminal" = "xfce4-terminal" ]; then
     xfce4-terminal --hide-menubar --hide-toolbar --hide-borders --geometry=70x34 -e "newsboat -r"
@@ -37,17 +42,45 @@ news() {
 # see more at: https://wiki.archlinux.org/index.php/ClamAV
 antivirus_up() {
   line
-  echo -e "\n$bold UPDATING THE ANTIVIRUS$reset$gray \n"
+  message "UPDATING THE ANTIVIRUS"
   sudo freshclam
   sudo clamav-unofficial-sigs.sh
   finish
 }
 
-# delete files inside Yay and Pacman cache
-cache_del() {
+# delete files inside Paru and Pacman cache
+paru_pacman_cache_del() {
   line
-  echo -e "\n$bold CLEANING YAY AND PACMAN CACHE$reset$gray \n"
-  yes S | sudo yay -Scc
+  message "CLEANING PARU AND PACMAN CACHE"
+  yes S | paru -Scc
+  finish
+}
+
+# clear trash
+clear_trash() {
+  line
+  message "CLEANING THE TRASH"
+  trash-empty 2
+  finish
+}
+
+# delete everything inside the cache folder in the user's directory
+del_cache_folder() {
+  line
+  message "CLEANING CACHE FOLDER IN $HOME"
+  echo "Size:"
+  du -sh ~/.cache/
+  echo -e "\nCleaning..."
+  rm -rf ~/.cache/*
+  finish
+}
+
+# clear yarn and npm cache
+clear_yarn_and_npm_cache() {
+  line
+  message "CLEANING YARN AND NPM CACHE"
+  yarn cache clean
+  npm cache clean --force
   finish
 }
 
@@ -55,9 +88,9 @@ cache_del() {
 # you need to install the 'reflector' program
 mirrorlist_up() {
   line
-  echo -e "\n$bold UPDATING THE MIRRORLIST$reset$gray \n"
-  sudo reflector -l 10 --sort rate --completion-percent 100 -a 24 -p https -c Brazil -c Germany -c Czechia -c France -c Sweden -c 'United States' --save /etc/pacman.d/mirrorlist
-  echo -e "\n$bold Update mirrorlist:$reset$gray \n"
+  message "UPDATING THE MIRRORLIST"
+  sudo reflector -l 10 --sort rate --completion-percent 100 -a 24 -p https --save /etc/pacman.d/mirrorlist
+  message "Update mirrorlist:"
   cat /etc/pacman.d/mirrorlist
   finish
 }
@@ -65,7 +98,7 @@ mirrorlist_up() {
 # update repository keys
 keys_up() {
   line
-  echo -e "\n$bold UPDATING REPOSITORY KEYS$reset$gray \n"
+  message "UPDATING REPOSITORY KEYS"
   sudo pacman-key --init
   sudo pacman-key --populate
   #sudo pacman -S --noconfirm archlinux-keyring
@@ -73,11 +106,12 @@ keys_up() {
 }
 
 # official repository update
-# you need to install the 'yay' program
+# you need to install the 'paru' program
 repository_up() {
   line
-  echo -e "\n$bold UPDATING OFFICIAL REPOSITORY$reset$gray \n"
-  yay -Syu --repo --noconfirm --cleanafter --overwrite '/usr/lib/python3.8/site-packages/*'
+  message "UPDATING OFFICIAL REPOSITORY"
+  yes S | paru -Syu --repo --cleanafter
+  # paru -Syu --repo --noconfirm --cleanafter
   finish
 }
 
@@ -85,7 +119,7 @@ repository_up() {
 # you need to install the 'flatpak'
 flatpak_up() {
   line
-  echo -e "\n$bold UPDATING FLATPAK$reset$gray \n"
+  message "UPDATING FLATPAK"
   sudo flatpak update --noninteractive
   finish
 }
@@ -94,7 +128,7 @@ flatpak_up() {
 # you need to install the 'snap'
 snap_up() {
   line
-  echo -e "\n$bold UPDATING SNAP$reset$gray \n"
+  message "UPDATING SNAP"
   sudo snap refresh
   finish
 }
@@ -102,23 +136,32 @@ snap_up() {
 # update the Arch User Repository
 aur_up() {
   line
-  echo -e "\n$bold UPDATING AUR$reset$gray \n"
-  yay -Syu --aur --noconfirm --cleanafter --overwrite '/usr/lib/python3.8/site-packages/*'
+  message "UPDATING AUR"
+  yes S | paru -Syu --aur --cleanafter
+  # paru -Syu --aur --noconfirm --cleanafter
   finish
 }
 
 # update the pkgfile data
 pkgfile_up() {
   line
-  echo -e "\n$bold UPDATING PKGFILE$reset$gray \n"
+  message "UPDATING PKGFILE"
   pkgfile -u
+  finish
+}
+
+# remove unneeded packages (orphans)
+orphans() {
+  line
+  message "REMOVING UNNEEDED PACKAGES (ORPHANS)"
+  yes S | paru --clean
   finish
 }
 
 # ask if you want to restart the system
 restart() {
   line
-  echo -e "\n$bold THE END$reset$gray \n"
+  message "THE END"
   read -p " Do you want to restart the system (y/n)? " -n 1
   if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo
@@ -133,14 +176,15 @@ Update_all_the_system/menu() {
   sudo echo && finish
   news
   antivirus_up
-  cache_del
+  paru_pacman_cache_del
   mirrorlist_up
   keys_up
   repository_up
   flatpak_up
   snap_up
   aur_up
-  pkgfile_up
+#  pkgfile_up
+  orphans
   restart
 }
 
@@ -148,10 +192,11 @@ Update_all_the_system/menu() {
 Update_only_the_oficial_repository/menu() {
   sudo echo && finish
   news
-  cache_del
+  paru_pacman_cache_del
   mirrorlist_up
   keys_up
   repository_up
+  orphans
   restart
 }
 
@@ -159,17 +204,27 @@ Update_only_the_oficial_repository/menu() {
 Update_only_the_AUR/menu() {
   sudo echo && finish
   news
-  cache_del
+  paru_pacman_cache_del
   mirrorlist_up
   keys_up
   aur_up
+  orphans
   restart
+}
+
+# MENU ITEM
+Clean_the_system/menu() {
+  sudo echo && finish
+  clear_trash
+  del_cache_folder
+  paru_pacman_cache_del
+  clear_yarn_and_npm_cache
 }
 
 echo
 while :; do           # infinite menu loop
   echo -e "$gray "
-  fmenu2              # create the network menu
+  fmenu2              # create the menu
   clear
   top
 done
